e2abe46b03b1d27795e8d67b68c8d4a1be918d6c
Priya Wagh <wpriya112@gmail.com>
2025-12-12 23:54:32 +0530

    Update main.tf

--- gcp-tf/main.tf
+++ gcp-tf/main.tf
@@ -1,19 +1,4 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = ">= 4.0"
-    }
-  }
-}
-
-provider "google" {
-  project = var.project_id
-  region  = var.region
-  zone    = var.zone
-}
-
-resource "google_compute_instance" "vm" {
+  resource "google_compute_instance" "vm" {
   name         = "jenkins-tf-vm"
   machine_type = "e2-medium"
   zone         = var.zone
@@ -26,16 +11,6 @@ resource "google_compute_instance" "vm" {
 
   network_interface {
     network = "default"
-
-    # Assign external IP
     access_config {}
-  }
-
-  labels = {
-    "goog-terraform-provisioned" = "true"
-  }
-
-  metadata = {
-    "startup-script" = "echo Hello from Terraform!"
   }
 }
